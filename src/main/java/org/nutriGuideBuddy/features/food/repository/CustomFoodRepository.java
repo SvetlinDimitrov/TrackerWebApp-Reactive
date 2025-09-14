ecf8bc255600db087cc8a0035ffdfeac2bc519b5
admin <svetlin.dimitrov@primeholding.com>
2025-09-14 23:11:52 +0300

    Add custom repositories for Food and User with filtering and pagination capabilities

---
+++ src/main/java/org/nutriGuideBuddy/features/food/repository/CustomFoodRepository.java
@@ -0,0 +1,47 @@
+package org.nutriGuideBuddy.features.food.repository;
+
+import static org.springframework.data.relational.core.query.Criteria.where;
+import static org.springframework.data.relational.core.query.Query.query;
+
+import java.util.Optional;
+import lombok.RequiredArgsConstructor;
+import org.nutriGuideBuddy.features.food.entity.*;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.r2dbc.core.R2dbcEntityTemplate;
+import org.springframework.data.r2dbc.repository.Modifying;
+import org.springframework.stereotype.Repository;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+@Repository
+@RequiredArgsConstructor
+public class CustomFoodRepository {
+
+  private final R2dbcEntityTemplate entityTemplate;
+
+  public Flux<Food> findAllFoodsByMealId(Long mealId) {
+    return entityTemplate.select(query(where("mealId").is(mealId)), Food.class);
+  }
+
+  public Mono<Page<Food>> findAllByFoodsByUserIdAndMealIdPageable(
+      Long userId, Long mealId, Pageable pageable) {
+    return Optional.ofNullable(mealId)
+        .map(
+            id ->
+                entityTemplate.select(
+                    query(where("userId").is(userId).and("mealId").is(mealId)), Food.class))
+        .orElse(
+            entityTemplate.select(
+                query(where("userId").is(userId).and("mealId").isNull()), Food.class))
+        .skip(pageable.getOffset())
+        .take(pageable.getPageSize())
+        .collectList()
+        .flatMap(
+            foodEntities ->
+                entityTemplate
+                    .count(query(where("userId").is(userId)), Food.class)
+                    .map(count -> new PageImpl<>(foodEntities, pageable, count)));
+  }
+}
---
+++ src/main/java/org/nutriGuideBuddy/features/user/repository/UserRepositoryCustomImpl.java
@@ -0,0 +1,95 @@
+package org.nutriGuideBuddy.features.user.repository;
+
+import static org.springframework.data.relational.core.query.Criteria.where;
+import static org.springframework.data.relational.core.query.Query.query;
+
+import java.util.Map;
+import lombok.RequiredArgsConstructor;
+import org.nutriGuideBuddy.features.user.dto.UserFilter;
+import org.nutriGuideBuddy.features.user.entity.User;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.r2dbc.core.R2dbcEntityTemplate;
+import org.springframework.data.relational.core.query.Criteria;
+import org.springframework.stereotype.Repository;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+@Repository
+@RequiredArgsConstructor
+public class UserRepositoryCustomImpl implements UserCustomRepository {
+
+  private final R2dbcEntityTemplate entityTemplate;
+
+  public Flux<User> findAllByFilter(UserFilter filter) {
+    var criteria = Criteria.empty();
+
+    if (filter.getUsername() != null && !filter.getUsername().isBlank()) {
+      criteria = criteria.and(where("username").like("%" + filter.getUsername() + "%"));
+    }
+
+    if (filter.getEmail() != null && !filter.getEmail().isBlank()) {
+      criteria = criteria.and(where("email").like("%" + filter.getEmail() + "%"));
+    }
+
+    if (filter.getRole() != null && !filter.getRole().isBlank()) {
+      criteria = criteria.and(where("user_role").is(filter.getRole()));
+    }
+
+    if (filter.getIdsIn() != null && !filter.getIdsIn().isEmpty()) {
+      criteria = criteria.and(where("id").in(filter.getIdsIn()));
+    }
+
+    if (filter.getIdsNotIn() != null && !filter.getIdsNotIn().isEmpty()) {
+      criteria = criteria.and(where("id").notIn(filter.getIdsNotIn()));
+    }
+
+    var constructedQuery = query(criteria);
+
+    var pageable = filter.getPageable();
+    for (Map.Entry<String, String> entry : pageable.getSort().entrySet()) {
+      String sortField = entry.getKey();
+      String sortDirection = entry.getValue().toUpperCase();
+
+      if ("ASC".equals(sortDirection)) {
+        constructedQuery = constructedQuery.sort(Sort.by(sortField).ascending());
+      } else if ("DESC".equals(sortDirection)) {
+        constructedQuery = constructedQuery.sort(Sort.by(sortField).descending());
+      } else {
+        System.out.println("Invalid sort direction: " + entry.getValue());
+      }
+    }
+
+    return entityTemplate
+        .select(User.class)
+        .matching(constructedQuery)
+        .all()
+        .skip((long) pageable.getPageNumber() * pageable.getPageSize())
+        .take(pageable.getPageSize());
+  }
+
+  public Mono<Long> countByFilter(UserFilter filter) {
+    var criteria = Criteria.empty();
+
+    if (filter.getUsername() != null && !filter.getUsername().isBlank()) {
+      criteria = criteria.and(where("username").like("%" + filter.getUsername() + "%"));
+    }
+
+    if (filter.getEmail() != null && !filter.getEmail().isBlank()) {
+      criteria = criteria.and(where("email").like("%" + filter.getEmail() + "%"));
+    }
+
+    if (filter.getRole() != null && !filter.getRole().isBlank()) {
+      criteria = criteria.and(where("user_role").is(filter.getRole()));
+    }
+
+    if (filter.getIdsIn() != null && !filter.getIdsIn().isEmpty()) {
+      criteria = criteria.and(where("id").in(filter.getIdsIn()));
+    }
+
+    if (filter.getIdsNotIn() != null && !filter.getIdsNotIn().isEmpty()) {
+      criteria = criteria.and(where("id").notIn(filter.getIdsNotIn()));
+    }
+
+    return entityTemplate.count(query(criteria), User.class);
+  }
+}
