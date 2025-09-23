ae6ba0a414530c8ef5b15edd075905ae5d537f70
admin <svetlin.dimitrov@primeholding.com>
2025-09-23 14:56:04 +0300

    Update TODO comments for nutrition API testing and nutrient RDI service

--- src/main/java/org/nutriGuideBuddy/NutriGuideBuddyApplication.java
+++ src/main/java/org/nutriGuideBuddy/NutriGuideBuddyApplication.java
@@ -7,7 +7,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class NutriGuideBuddyApplication {
 
   // TODO:: CUSTOM FOOD FEATURE
-  // TODO:: FIX NUTRITION API
+  // TODO:: TEST MORE NUTRITION API
+  // TODO:: TEST NUTRIENT RDI SERVICE AND DIETS AND NUTRIENT AUTHORITY
   public static void main(String[] args) {
     SpringApplication.run(NutriGuideBuddyApplication.class, args);
   }
